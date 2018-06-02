7de3a23455a940e87b6b9e18411574d368089789
Leandro Coutinho <lbcoutinho.blm@gmail.com>
2018-06-02 20:45:47 -0300

    Fix index.js for Heroku

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
-const express = require('express');
-
-const app = express();
-
-app.use(express.static('public'));
+const express = require("express");
+const path = require("path");
 
 const port = process.env.PORT || 8080;
-app.listen(port, () => {
-  console.log(`Server listening on port ${port}`);
-});
+
+express()
+  .use(express.static(path.join(__dirname, "public")))
+  .listen(port, () => {
+    console.log(`Server listening on port ${port}`);
+  });
