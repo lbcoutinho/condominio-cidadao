23c3be2315cbff1e9d21bc564856c88e12fb7458
Leandro Coutinho <lbcoutinho.blm@gmail.com>
2018-06-03 22:36:02 -0300

    Added question for test

--- public/js/index.js
+++ public/js/index.js
@@ -1,15 +1,44 @@
-{/* <a href="#images" class="btn" id="view-work">
-  View Work
-</a>; */}
+$(() => {
+  $("#startTest").click(function() {
+    localStorage.correct = 0;
+  });
 
-// Animate Smooth Scroll
-$("#view-work").on("click", function() {
-  const images = $("#images").position().top;
+  function validateAndSaveOption(btnId, inputName, correctOption) {
+    $(`#${btnId}`).click(e => {
+      const selected = $(`input[name=${inputName}]:checked`).val();
+      if (!selected) {
+        e.preventDefault();
+        alert("Por favor, selecione uma opção.");
+      } else if (selected == correctOption) {
+        localStorage.correct = parseInt(localStorage.correct) + 1;
+      }
+    });
+  }
 
-  $("html, body").animate(
-    {
-      scrollTop: images
-    },
-    900
-  );
+  validateAndSaveOption("btn1", "question1", "1a");
+  validateAndSaveOption("btn2", "question2", "2b");
+  validateAndSaveOption("btn3", "question3", "3d");
+
+  const result = $("#result");
+
+  if (result.length) {
+    const correct = parseInt(localStorage.correct);
+    if (correct == 0) {
+      result.html(
+        "Infelizmente você não acertou nenhuma resposta. Sugerimos que leia novamente com atenção os valores e princípios do Manifesto e tente novamente."
+      );
+    } else if (correct == 1) {
+      result.html(
+        "Você acertou apenas 1 resposta. Sugerimos que leia novamente os valores e princípios do Manifesto e tente novamente."
+      );
+    } else if (correct == 2) {
+      result.html(
+        "Você acertou 2 respostas. É um bom resultado, mas sugerimos que leia novamente os valores e princípios do Manifesto para esclarecer qualquer dúvida."
+      );
+    } else if (correct == 3) {
+      result.html(
+        "Parabéns! Você acertou todas as respostas e está no caminho certo para tornar o seu condomínio mais cidadão."
+      );
+    }
+  }
 });
